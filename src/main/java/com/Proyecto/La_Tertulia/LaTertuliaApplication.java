a1f5f8660e59236057524fc0266b21edef666f39
David Lotero <Davidloteria@gmail.com>
2025-05-05 09:05:41 -0500

    add: Agregar la estructura basica del proyecto

---
+++ src/main/java/com/Proyecto/La_Tertulia/LaTertuliaApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto.La_Tertulia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaTertuliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaTertuliaApplication.class, args);
+	}
+
+}
